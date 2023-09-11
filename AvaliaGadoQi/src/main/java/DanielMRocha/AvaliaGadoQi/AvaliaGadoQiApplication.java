c1c8bfae37867d42445c29554d9f5ac0c7795edc
Daneil_M_R <danroch1913@gmail.com>
2023-09-11 18:06:35 -0300

    Inicio, descompactação do arquivo criado no spring.io

---
+++ AvaliaGadoQi/src/main/java/DanielMRocha/AvaliaGadoQi/AvaliaGadoQiApplication.java
@@ -0,0 +1,13 @@
+package DanielMRocha.AvaliaGadoQi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvaliaGadoQiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvaliaGadoQiApplication.class, args);
+	}
+
+}
---
+++ AvaliaGadoQi/src/test/java/DanielMRocha/AvaliaGadoQi/AvaliaGadoQiApplicationTests.java
@@ -0,0 +1,13 @@
+package DanielMRocha.AvaliaGadoQi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvaliaGadoQiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
